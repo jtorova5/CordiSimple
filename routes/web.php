f868cc0236ab031f9b2f86cb4ed1a7ca107e37d9
EloySad <johaneloysanchezhernandez@gmail.com>
2024-11-01 09:58:09 -0500

    feat: Continue fixing error in the reservation's controller

--- routes/web.php
+++ routes/web.php
@@ -31,26 +31,12 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Rutas para usuarios regulares (User)
-Route::middleware(['auth:web'])->group(function () {
-    // Reservas
-    Route::get('reservations', [ReservationController::class, 'index'])->name('reservations.index');
-    Route::get('reservations/create', [ReservationController::class, 'create'])->name('reservations.create');
-    Route::post('reservations', [ReservationController::class, 'store'])->name('reservations.store');
-    Route::get('reservations/{id}', [ReservationController::class, 'show'])->name('reservations.show');
-    Route::get('reservations/{id}/edit', [ReservationController::class, 'edit'])->name('reservations.edit');
-    Route::put('reservations/{id}', [ReservationController::class, 'update'])->name('reservations.update');
-    Route::delete('reservations/{id}', [ReservationController::class, 'destroy'])->name('reservations.destroy');
-});
+Route::get('reservations', [ReservationController::class, 'index'])->name('reservations.index');
+Route::get('reservations/create', [ReservationController::class, 'create'])->name('reservations.create');
+Route::post('reservations', [ReservationController::class, 'store'])->name('reservations.store');
+Route::get('reservations/{id}', [ReservationController::class, 'show'])->name('reservations.show');
+Route::get('reservations/{id}/edit', [ReservationController::class, 'edit'])->name('reservations.edit');
+Route::put('reservations/reservation', [ReservationController::class, 'update'])->name('reservations.update');
+Route::delete('reservations/{id}', [ReservationController::class, 'destroy'])->name('reservations.destroy');
 
-// Rutas para administradores (Admin)
-Route::middleware(['auth:admin'])->group(function () {
-    // Eventos
-    Route::get('/events', [EventController::class, 'index'])->name('events.index');
-    Route::get('/events/create', [EventController::class, 'create'])->name('events.create');
-    Route::post('/events', [EventController::class, 'store'])->name('events.store');
-    Route::get('/events/{id}', [EventController::class, 'show'])->name('events.show');
-    Route::get('/events/{id}/edit', [EventController::class, 'edit'])->name('events.edit');
-    Route::put('/events/{id}', [EventController::class, 'update'])->name('events.update');
-    Route::delete('/events/{id}', [EventController::class, 'destroy'])->name('events.destroy');
-});
+require __DIR__ . '/auth.php';

a9886d42b9345ccc4b3e70d829a36e73effce64d
NicolasMC07 <nicolasmarcar2006@gmail.com>
2024-11-01 11:32:54 -0500

    feat: updatading RegisteredUserController configuration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,12 +34,12 @@ class RegisteredUserController extends Controller
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'address' => ['required','string','max:255'],
             'phone_number' => ['required','string','max:255'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'confirmed', 'min:4'],
         ]);
 
         $user = User::create([
             'name' => $request->name,
-            'email' => $request->email,
+            'email' => $request->email, 
             'address' => $request->address,
             'phone_number' => $request->phone_number,
             'password' => Hash::make($request->password),
